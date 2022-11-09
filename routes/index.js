aaca6889da6a18ab3d5012e1360da129ebf05a2c
ihascats <s.pavlovic.rs@gmail.com>
2022-11-09 23:56:39 +0100

    Add a "guide" with route "directions" on "/"

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,31 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.send(req.user);
+  res.send({
+    get: [
+      '/user',
+      '/friend_requests',
+      '/timeline',
+      '/users',
+      '/users:<id>',
+      '/auth/google',
+      '/post/<post_id>',
+      '/post/comment:<comment_id>',
+      '/post/user:<user_id>',
+    ],
+    post: ['/post', '/post/comment:<post_id>'],
+    put: [
+      '/post/<post_id>',
+      '/post/like:<post_id>',
+      '/post/comment/:<comment_id>',
+      '/post/comment/like:<comment_id>',
+      '/user',
+      '/fr:<user_id>',
+      '/accept_fr:<user_id>',
+      '/decline_fr:<user_id>',
+    ],
+    delete: ['/user', '/post/<post_id>', '/post/comment/:<comment_id>'],
+  });
 });
 
 router.get('/timeline', verifyToken, get_timeline);
