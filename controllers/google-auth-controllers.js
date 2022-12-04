243ff9b0f875f27f77e71b62c3a7c918266567cb
ihascats <s.pavlovic.rs@gmail.com>
2022-12-04 18:19:28 +0100

    Fix google auth success redirect

--- controllers/google-auth-controllers.js
+++ controllers/google-auth-controllers.js
@@ -3,7 +3,7 @@ const { signToken } = require('./jwt-controllers');
 exports.redirect_success = function (req, res) {
   // Successful authentication, redirect home.
   res.redirect(
-    `${process.env.CLIENT}/signIn/${signToken(
+    `${process.env.CLIENT}/signUp/${signToken(
       { user: req.user },
       {
         expiresIn: '16d',
